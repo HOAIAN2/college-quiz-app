01cf7f58be3c52a5d84cb166163b0a41ca143b39
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-09-25 19:49:35 +0700

    Prepare setup lang

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /**
+     * Login
+     */
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8'],
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if ($user && $user->is_active == false)  return response()->json([
+            'message' => 'Account Disabled'
+        ], 400);
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Email or password incorrect'
+            ], 400);
+        }
+        $token = $user->createToken('My Token')->plainTextToken;
+        return response()->json([
+            /** @var  User> */
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+    /**
+     * Logout
+     */
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'Token revoked'
+        ]);
+    }
+    /**
+     * Change password
+     */
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|string|min:8|confirmed',
+        ]);
+        $user = $request->user();
+        if (!Hash::check($request->current_password, $user->password)) {
+            return response()->json([
+                'message' => 'Password incorrect'
+            ], 400);
+        }
+        $user->update([
+            'password' => Hash::make($request->new_password),
+        ]);
+        $user->tokens()->delete();
+        return response()->json([
+            'message' => 'Tokens revoked'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/auth')->controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('logout', 'logout')->middleware('auth:sanctum');
+    Route::post('change-password', 'changePassword')->middleware('auth:sanctum');
 });
