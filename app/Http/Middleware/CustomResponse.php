9224df1ff513ef43f76169384432fe8d20c9b2a0
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-09-26 12:47:17 +0700

    Update camel case

---
+++ app/Http/Middleware/CustomResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Resources\CustomResource;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        $content_type = $response->headers->get('content-type');
+        if ($content_type === 'application/json') {
+            $content = CustomResource::toCamelCase(json_decode($response->getContent(), true));
+            $response->setContent(json_encode($content));
+        }
+        return $response;
+    }
+}
