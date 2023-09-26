cb763a5e9609b0fec0f46d3187b567b1027eb75b
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-09-26 16:57:50 +0700

    Convert type to undefined

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -1,9 +1,9 @@
 import { createContext, useReducer, ReactNode, Reducer, Dispatch } from 'react'
 import { User } from '../models/user'
 
-const init: unknown = null
+const init: unknown = undefined
 interface UserType {
-    user: User | null
+    user: User | undefined
     dispatchUser: Dispatch<UserAction>
 }
 const UserContext = createContext<UserType>(init as UserType)
@@ -13,9 +13,9 @@ const USER_ACTION = {
 }
 interface UserAction {
     type: string
-    payload: User | null
+    payload: User | undefined
 }
-const userReducer: Reducer<User | null, UserAction> = (state, action) => {
+const userReducer: Reducer<User | undefined, UserAction> = (state, action) => {
     switch (action.type) {
         case USER_ACTION.SET: {
             if (!action.payload) return state
@@ -29,13 +29,13 @@ const userReducer: Reducer<User | null, UserAction> = (state, action) => {
             }
         }
         case USER_ACTION.REMOVE:
-            return null
+            return undefined
         default:
             return state;
     }
 }
 function UserProvider({ children }: { children: ReactNode }) {
-    const [user, dispatchUser] = useReducer(userReducer, null)
+    const [user, dispatchUser] = useReducer(userReducer, undefined)
     return (
         <UserContext.Provider value={{ user, dispatchUser } as UserType}>
             {children}
