826914f35eab62f077228ae94a1a32acd173cd1f
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-10-06 19:45:01 +0700

    Update css

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,3 +1,11 @@
+import {
+    RiAddFill
+} from 'react-icons/ri'
+import {
+    BiImport,
+    BiExport
+} from 'react-icons/bi'
+
 export default function Dashboard() {
     return (
         <div
@@ -6,6 +14,34 @@ export default function Dashboard() {
                     'dashboard-d'
                 ].join(' ')
             }
-        >Dashboard</div>
+        >
+            <div className={
+                [
+                    'action-bar-d'
+                ].join(' ')
+            }>
+                <div className={
+                    [
+                        'action-item-d'
+                    ].join(' ')
+                }>
+                    <RiAddFill /> Add
+                </div>
+                <div className={
+                    [
+                        'action-item-d-white'
+                    ].join(' ')
+                }>
+                    <BiImport /> Import
+                </div>
+                <div className={
+                    [
+                        'action-item-d-white'
+                    ].join(' ')
+                }>
+                    <BiExport /> Export
+                </div>
+            </div>
+        </div>
     )
 }
