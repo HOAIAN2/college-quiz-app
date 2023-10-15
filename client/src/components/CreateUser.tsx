75ccd1155e2cd2b9270e3c0882dd2fc6c4e6d6bd
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-10-15 15:47:27 +0700

    Migrate to react-query

--- client/src/components/CreateUser.tsx
+++ client/src/components/CreateUser.tsx
@@ -4,6 +4,7 @@ import {
 } from 'react-icons/rx'
 import styles from '../styles/CreateUser.module.css'
 import { reqCreateUser } from '../utils/user'
+import { useMutation } from '@tanstack/react-query'
 
 interface CreateUserProps {
     type?: 'student' | 'teacher' | 'admin'
@@ -14,25 +15,27 @@ export default function CreateUser({
 }: CreateUserProps) {
     const [hide, setHide] = useState(true)
     const handleTurnOffInsertMode = () => {
+        const transitionTiming = getComputedStyle(document.documentElement).getPropertyValue('--transition-timing-fast')
         setHide(true)
         setTimeout(() => {
             setInsertMode(false)
-        }, 400)
+        }, parseInt(transitionTiming.substring(0, transitionTiming.length)))
     }
-    const handleCreateUser = (e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
+    const handleCreateUser = async (e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
         e.preventDefault()
         const submitter = e.nativeEvent.submitter as HTMLButtonElement
-        const form = e.currentTarget
+        const form = e.target as HTMLFormElement
         const formData = new FormData(form)
-        reqCreateUser(formData)
-            .then(() => {
-                if (submitter.name === 'save') handleTurnOffInsertMode()
-                else form.reset()
-            })
-            .catch(e => {
-                console.log(e)
-            })
+        await reqCreateUser(formData)
+        if (submitter.name === 'save') handleTurnOffInsertMode()
+        else form.reset()
     }
+    const { mutate } = useMutation({
+        mutationFn: handleCreateUser,
+        onError: (e) => {
+            console.log(e)
+        },
+    })
     useEffect(() => {
         setHide(false)
     }, [])
@@ -57,7 +60,9 @@ export default function CreateUser({
                         <RxCross2 />
                     </div>
                 </div>
-                <form onSubmit={handleCreateUser} className={styles['form-data']}>
+                <form onSubmit={(e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
+                    mutate(e)
+                }} className={styles['form-data']}>
                     <div className={
                         [
                             styles['group-inputs']
