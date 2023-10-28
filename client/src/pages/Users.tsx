09ef2930fb15125f9ae3c84ed5d0b0f46eca33d4
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-10-28 09:51:28 +0700

    Update Page

--- client/src/pages/Users.tsx
+++ client/src/pages/Users.tsx
@@ -10,6 +10,10 @@ import {
     GiFemale,
     GiMale
 } from 'react-icons/gi'
+import {
+    GrFormNext,
+    GrFormPrevious,
+} from 'react-icons/gr'
 import CreateUser from '../components/CreateUser'
 import { UsersLanguage } from '../models/lang'
 import { useLanguage } from '../contexts/hooks'
@@ -33,13 +37,17 @@ export default function Users({
         queryFn: () => reqGetUsersByType('admin', Number(searchParams.get('page')))
     })
     useEffect(() => {
+        if (!searchParams.has('page')) {
+            searchParams.set('page', '1')
+            setSearchParams(searchParams)
+        }
         fetch(`/langs/page.users.${appLanguage}.json`)
             .then(res => res.json())
             .then((data) => {
                 setLanguage(data)
             })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [appLanguage])
-    console.log(queryData)
     return (
         <>
             {insertMode === true ?
@@ -112,6 +120,46 @@ export default function Users({
                                 }
                             </tbody>
                         </table>
+                        <div className={styles['table-links']}>
+                            {
+                                !queryData.isError && queryData.data ? (
+                                    <>
+                                        {queryData.data.links.map(link => {
+                                            if (isNaN(Number(link.label))) return (
+                                                <button key={type + link.label} className={
+                                                    [
+                                                        styles['next-previous']
+                                                    ].join(' ')
+                                                }
+                                                    onClick={() => {
+                                                        if (!link.url) return
+                                                        const url = new URL(link.url)
+                                                        searchParams.set('page', url.searchParams.get('page') || '1')
+                                                        setSearchParams(searchParams)
+                                                    }}
+                                                >
+                                                    {link.label.includes('Next') ? <GrFormNext /> : <GrFormPrevious />}
+                                                </button>
+                                            )
+                                            return (
+                                                <button key={type + link.label} className={
+                                                    [
+                                                        'button-d',
+                                                        !link.active ? 'inactive' : ''
+                                                    ].join(' ')
+                                                }
+                                                    onClick={() => {
+                                                        if (!link.url) return
+                                                        const url = new URL(link.url)
+                                                        searchParams.set('page', url.searchParams.get('page') || '1')
+                                                        setSearchParams(searchParams)
+                                                    }}
+                                                >{link.label}</button>
+                                            )
+                                        })}
+                                    </>) : null
+                            }
+                        </div>
                     </div>
                 </div>
             </div >
