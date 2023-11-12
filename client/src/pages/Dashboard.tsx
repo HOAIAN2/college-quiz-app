f55bdebfa96b5567f1c5712f49752b103baaa967
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-11-12 13:44:58 +0700

    Update CSS for Dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,22 @@
+import { useQuery } from '@tanstack/react-query'
+import { reqGetDashboard } from '../utils/dashboard'
+import {
+    PiStudent,
+    PiChalkboardTeacherLight,
+    PiExam,
+} from 'react-icons/pi'
+import {
+    SiGoogleclassroom
+} from 'react-icons/si'
 import styles from '../styles/Dashboard.module.css'
 
 export default function Dashboard() {
+    const queryData = useQuery({
+        queryKey: ['dashboard'],
+        queryFn: reqGetDashboard
+    })
+    if (queryData.isLoading) return <div className={styles['loading']}>Loading...</div>
+    if (queryData.isError) return <div className={styles['loading']}>Loading...</div>
     return (
         <div
             className={
@@ -17,28 +33,52 @@ export default function Dashboard() {
                         styles['dashboard-item'],
                         styles['container-blue']
                     ].join(' ')
-                }></div>
+                }>
+                    <div className={styles['item-left']}>
+                        <PiStudent />
+                    </div>
+                    <div className={styles['item-top']}>{queryData.data?.studentCount}</div>
+                    <div className={styles['item-bottom']}>Số lượng học sinh</div>
+                </div>
                 <div className={
                     [
                         'dashboard-item-d',
                         styles['dashboard-item'],
                         styles['container-red']
                     ].join(' ')
-                }></div>
+                }>
+                    <div className={styles['item-left']}>
+                        <PiChalkboardTeacherLight />
+                    </div>
+                    <div className={styles['item-top']}>{queryData.data?.teacherCount}</div>
+                    <div className={styles['item-bottom']}>Số lượng giáo viên</div>
+                </div>
                 <div className={
                     [
                         'dashboard-item-d',
                         styles['dashboard-item'],
                         styles['container-green']
                     ].join(' ')
-                }></div>
+                }>
+                    <div className={styles['item-left']}>
+                        <SiGoogleclassroom />
+                    </div>
+                    <div className={styles['item-top']}>{queryData.data?.courseCount}</div>
+                    <div className={styles['item-bottom']}>Khóa học hiện tại</div>
+                </div>
                 <div className={
                     [
                         'dashboard-item-d',
                         styles['dashboard-item'],
                         styles['container-pink']
                     ].join(' ')
-                }></div>
+                }>
+                    <div className={styles['item-left']}>
+                        <PiExam />
+                    </div>
+                    <div className={styles['item-top']}>{queryData.data?.examInThisMonth}</div>
+                    <div className={styles['item-bottom']}>Bài thi tháng này</div>
+                </div>
             </div>
         </div>
     )
