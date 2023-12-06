cc757a572a416e49ea40a8c140894d10eab6de7d
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-12-06 13:18:12 +0700

    Update Compress command

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 rm -rf vendor
-rm app.zip
+#rm app.zip
+rm app.tar.gz
 composer install --optimize-autoloader --no-dev
 php artisan config:clear
 php artisan route:clear
 php artisan cache:clear
-zip -r app . -x "/node_modules/*" "/client/*" "/dump/*" "/img/*" "/.git/*" "composer.lock"
+# Zip only work on Linux
+#zip -r app . -x "/node_modules/*" "/client/*" "/dump/*" "/img/*" "/.git/*" "composer.lock"
+tar --exclude=client --exclude=dump --exclude=node_modules --exclude=img --exclude=.git --exclude=composer.lock --exclude='app.tar.gz' -czvf app.tar.gz *
