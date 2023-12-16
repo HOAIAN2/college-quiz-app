97f2bc1cc0ba01ef53e8f9a62658bbaeb3878667
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-12-16 19:10:47 +0700

    Add Middlware parse accept content type

---
+++ app/Http/Middleware/AcceptContentType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Str;
+
+class AcceptContentType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Str::startsWith($request->path(), 'api')) $request->headers->set('accept', 'application/json');
+        return $next($request);
+    }
+}
