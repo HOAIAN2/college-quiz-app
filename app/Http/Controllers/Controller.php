1691e61c2d0246693b999a0ecaffe245fb181216
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-12-17 19:03:55 +0700

    Optimize performance

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,40 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Str;
-
-
-/**
- * @property string[] $supported_languages
- */
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
-    public $supported_languages;
-    public $current_language;
-
-    function __construct()
+    public function __construct()
     {
-        $this->setLanguage();
-    }
-    private function setLanguage()
-    {
-        $this->current_language = App::getLocale();
-    }
-    private function getAvailableLanguages()
-    {
-        $langPath = resource_path('lang');
-        $availableLanguages = array_filter(glob($langPath . '/*'), 'is_dir');
-        $availableLanguages = array_map(function ($path) {
-            return basename($path);
-        }, $availableLanguages);
-        return $availableLanguages;
     }
     public function getUser()
     {
--- app/Http/Middleware/Locale.php
+++ app/Http/Middleware/Locale.php
@@ -17,22 +17,10 @@ class Locale
     public function handle(Request $request, Closure $next): Response
     {
         $accept_language = $request->header('Accept-Language');
-        if (Str::startsWith($accept_language, 'en')) app()->setLocale('en');
-        $supported_languages = $this->getAvailableLanguages();
-        if (in_array($accept_language, $supported_languages)) {
-            app()->setLocale($accept_language);
+        if ($accept_language != null) {
+            if (Str::startsWith($accept_language, 'en')) app()->setLocale('en');
+            else app()->setLocale($accept_language);
         }
-        if (Str::startsWith($accept_language, 'en')) app()->setLocale('en');
-        else app()->setLocale(env('LOCALE', 'vi'));
         return $next($request);
-    }
-    private function getAvailableLanguages()
-    {
-        $langPath = resource_path('lang');
-        $availableLanguages = array_filter(glob($langPath . '/*'), 'is_dir');
-        $availableLanguages = array_map(function ($path) {
-            return basename($path);
-        }, $availableLanguages);
-        return $availableLanguages;
     }
 }
