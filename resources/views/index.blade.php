3aac954fa6f654f837ebc448dfbc927145fd6573
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-12-24 13:28:43 +0700

    Fix Css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,8 +18,8 @@
   <link rel="preload" href="/fonts/AlumniSans-Medium.ttf" as="font" crossorigin>
   <link rel="preload" href="/fonts//AlumniSans-Bold.ttf" as="font" crossorigin>
   <title>{{$title}}</title>
-  <script type="module" crossorigin src="/assets/index-Rs0Cy-VG.js"></script>
-  <link rel="stylesheet" crossorigin href="/assets/index-BOlBdXNx.css">
+  <script type="module" crossorigin src="/assets/index-VzR90PYS.js"></script>
+  <link rel="stylesheet" crossorigin href="/assets/index-asMXa91X.css">
 </head>
 
 <body>
