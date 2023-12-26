599afa9f8fea18122c8a2d70a82bd8e6f771dff9
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-12-26 20:41:10 +0700

    Add Chapter Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             SubjectSeeder::class,
+            ChapterSeeder::class,
             StudentSeeder::class
         ]);
     }
