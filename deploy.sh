ab974adf2dfca553d4fefddae0671c60f7dace07
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2023-12-28 16:13:32 +0700

    Revert change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,10 @@
 #!/bin/sh
-read -p "Do you want to ignore vendor folder? (yes/no): " response
-
-response=${response:-yes}
-start_time=$(date +%s)
-
+rm -rf vendor
+rm app.tar.gz
+composer install --optimize-autoloader --no-dev
 php artisan config:clear
 php artisan route:clear
 php artisan cache:clear
-rm ./storage/logs/laravel.log
-rm -rf ./storage/framework/sessions/*
-
-# Check the user's response
-if [ "$response" == "yes" ]; then
-    rm app.tar.gz
-    tar --exclude=vendor --exclude=client --exclude=dump --exclude=node_modules --exclude=img --exclude=.git --exclude=composer.lock --exclude=vendor.tar.gz --exclude='app.tar.gz' -czvf app.tar.gz *
-    end_time=$(date +%s)
-    elapsed_time=$((end_time - start_time))
-    echo -e "Script executed in \e[1m$elapsed_time\e[0m seconds."
-else
-    rm -rf vendor
-    composer install --optimize-autoloader --no-dev
-    find . -type f -name '*.tar.gz' -exec rm {} +
-    tar --exclude=client --exclude=dump --exclude=node_modules --exclude=img --exclude=.git --exclude=composer.lock --exclude='app.tar.gz' -czvf app.tar.gz *
-    end_time=$(date +%s)
-    elapsed_time=$((end_time - start_time))
-    echo -e "Script executed in \e[1m$elapsed_time\e[0m seconds."
-fi
+rm storage/logs/laravel.log
+rm -rf storage/framework/sessions/*
+tar --exclude=client --exclude=dump --exclude=node_modules --exclude=img --exclude=.git --exclude=composer.lock --exclude='app.tar.gz' -czvf app.tar.gz *
