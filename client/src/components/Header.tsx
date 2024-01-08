415da255c87ce1537fab590c43278c8569d87a92
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-01-08 21:11:11 +0700

    Change header to drop list

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import {
     BiLogOut
 } from 'react-icons/bi'
@@ -16,11 +17,15 @@ import navBarStyles from '../styles/NavBar.module.css'
 export default function Header() {
     const { DOM, user, appLanguage } = useAppContext()
     const navigate = useNavigate()
+    const dropContainerRef = useRef<HTMLDivElement>(null)
     const handleLogout = () => {
         apiLogout()
             .finally(() => {
                 navigate(0)
             })
+    }
+    const handleToggle = () => {
+        dropContainerRef.current?.classList.toggle(styles['show'])
     }
     const fullName = appLanguage.language === 'vi'
         ? [
@@ -44,13 +49,22 @@ export default function Header() {
                 <h1 ref={DOM.titleRef} className={styles['app-title']}></h1>
             </div>
             <div className={styles['right-items']}>
-                <Link to='/profile' className={styles['right-item']}
+                {/* <Link to='/profile' className={styles['right-item']}
                     title={fullName}>
                     <FaRegUser />
-                </Link>
-                <span onClick={handleLogout} className={styles['right-item']}>
-                    <BiLogOut />
+                </Link> */}
+                <span onClick={handleToggle} className={styles['right-item']}>
+                    <FaRegUser />
                 </span>
+                <div onClick={handleToggle} className={styles['drop-down']} ref={dropContainerRef}>
+                    <Link to='/profile' className={styles['drop-item']}
+                        title={fullName}>
+                        <FaRegUser />
+                    </Link>
+                    <div onClick={handleLogout} className={styles['drop-item']}>
+                        <BiLogOut />
+                    </div>
+                </div>
             </div>
         </div>
     )
