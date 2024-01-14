f354d5cb024dae7c004e07d2d8d14b3010287414
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-01-14 08:55:49 +0700

    Clean code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,24 +24,29 @@ class Controller extends BaseController
     }
     public function clearUnusedTokens()
     {
-        $interval = env('CLEAR_TOKENS_INTERVAL', 60);
+        $clear_token_interval = env('CLEAR_TOKENS_INTERVAL', 60);
         $now = Carbon::now();
+        $is_api_call = Str::startsWith(request()->path(), 'api');
+
         $last_clear_tokens_at = Cache::has('last_clear_tokens_at')
             ? Carbon::parse(Cache::get('last_clear_tokens_at'))
             : $now;
         if ($last_clear_tokens_at == $now) Cache::put('last_clear_tokens_at', $now->format('Y-m-d H:i:s'));
-        if ($now->diffInSeconds($last_clear_tokens_at) > $interval) {
-            if (env('TOKEN_LIFETIME') != null && Str::startsWith(request()->path(), 'api')) {
-                $interval = Carbon::now()->subMinutes(env('TOKEN_LIFETIME'));
-                DB::table('personal_access_tokens')
-                    ->where(function ($query) use ($interval) {
-                        $query->where('last_used_at', '<', $interval)
-                            ->orWhere(function ($query) use ($interval) {
-                                $query->where('created_at', '<', $interval)
-                                    ->whereNull('last_used_at');
-                            });
-                    })->delete();
-            }
+
+        if (
+            $now->diffInSeconds($last_clear_tokens_at) > $clear_token_interval
+            && env('TOKEN_LIFETIME') != null
+            && $is_api_call == true
+        ) {
+            $interval = Carbon::now()->subMinutes(env('TOKEN_LIFETIME'));
+            DB::table('personal_access_tokens')
+                ->where(function ($query) use ($interval) {
+                    $query->where('last_used_at', '<', $interval)
+                        ->orWhere(function ($query) use ($interval) {
+                            $query->where('created_at', '<', $interval)
+                                ->whereNull('last_used_at');
+                        });
+                })->delete();
             Cache::put('last_clear_tokens_at', $now->format('Y-m-d H:i:s'));
         }
     }
