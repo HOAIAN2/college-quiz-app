ac163147f5730065ec1d91a3ac303bebc041b696
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-01-17 15:10:46 +0700

    Add Loading state for Roel Permission page

--- client/src/pages/RolePermissions.tsx
+++ client/src/pages/RolePermissions.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
 import { apiGetRolePermissions } from '../api/role-permission'
+import Loading from '../components/Loading'
+import styles from '../styles/RolePermissions.module.css'
 
 export default function RolePermissions() {
     const { id } = useParams()
@@ -10,6 +12,17 @@ export default function RolePermissions() {
     })
     console.log(queryData.data)
     return (
-        <div>RolePermissions</div>
+        <div
+            className={
+                [
+                    'dashboard-d',
+                    styles['role-permission-container']
+                ].join(' ')
+            }
+        >
+            {queryData.isLoading ?
+                <Loading />
+                : null}
+        </div>
     )
 }
