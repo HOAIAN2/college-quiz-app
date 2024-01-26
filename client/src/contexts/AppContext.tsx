8cff3dafe71e1971f52ad64a48f1bb08a2196ea0
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-01-26 16:49:30 +0700

    Clean AppContext code

--- client/src/contexts/AppContext.tsx
+++ client/src/contexts/AppContext.tsx
@@ -2,58 +2,45 @@ import { ReactNode, createContext, useRef, useState } from 'react'
 import { User } from '../models/user'
 import { getLanguage } from '../utils/languages'
 
-type AppContextType = {
-    DOM: {
-        sideBarRef: React.RefObject<HTMLDivElement>
-        titleRef: React.RefObject<HTMLHeadingElement>
-    }
-    appLanguage: {
-        language: string
-        setLanguage: React.Dispatch<React.SetStateAction<string>>
-    },
-    user: {
-        user: User | undefined
-        setUser: React.Dispatch<React.SetStateAction<User | undefined>>
-    }
-    permissions: {
-        items: string[]
-        setItems: React.Dispatch<React.SetStateAction<string[]>>
-        has: (name: string) => boolean
-    }
-}
+type AppContextType = ReturnType<typeof useAppContext>
 
 const init: unknown = {}
 export const AppContext = createContext<AppContextType>(init as AppContextType)
 
-export function AppProvider({ children }: { children: ReactNode }) {
+function useAppContext() {
+    const sideBarRef = useRef<HTMLDivElement>(null)
+    const titleRef = useRef<HTMLHeadingElement>(null)
     const [language, setLanguage] = useState(getLanguage())
     const [permissions, setPermissions] = useState<string[]>([])
     const [user, setUser] = useState<User | undefined>()
-    const sideBarRef = useRef<HTMLDivElement>(null)
-    const titleRef = useRef<HTMLHeadingElement>(null)
+
+    return {
+        DOM: {
+            sideBarRef,
+            titleRef
+        },
+        appLanguage: {
+            language,
+            setLanguage
+        },
+        user: {
+            user,
+            setUser
+        },
+        permissions: {
+            items: permissions,
+            setItems: setPermissions,
+            has: (name: string) => permissions.includes(name)
+        }
+    };
+}
+
+export function AppProvider({ children }: { children: ReactNode }) {
+    const contextValue = useAppContext()
+
     return (
-        <AppContext.Provider value={
-            {
-                DOM: {
-                    sideBarRef,
-                    titleRef
-                },
-                appLanguage: {
-                    language,
-                    setLanguage
-                },
-                user: {
-                    user,
-                    setUser
-                },
-                permissions: {
-                    items: permissions,
-                    setItems: setPermissions,
-                    has: (name: string) => permissions.includes(name)
-                }
-            }
-        }>
+        <AppContext.Provider value={contextValue}>
             {children}
-        </AppContext.Provider >
-    )
+        </AppContext.Provider>
+    );
 }
