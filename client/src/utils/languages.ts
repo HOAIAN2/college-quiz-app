60beab12697231ccbea9971941199051bc961ee5
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-01-27 14:31:25 +0700

    Update LANG_KEY

--- client/src/utils/languages.ts
+++ client/src/utils/languages.ts
@@ -1,10 +1,12 @@
+import { LANG_KEY } from '../config/env'
+
 const languagesSupported = [
     'vi',
     'en'
 ]
 
 export function getLanguage() {
-    const localLanguage = localStorage.getItem('lang') as string
+    const localLanguage = localStorage.getItem(LANG_KEY) as string
     if (languagesSupported.includes(localLanguage)) return localLanguage
     const language = navigator.language.includes('en') ? 'en' : navigator.language
     if (languagesSupported.includes(language)) return language
