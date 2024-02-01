1557848f33bb7bacc5ae3b9d6732cd11cddcbe50
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-02-01 11:47:26 +0700

    Change logic custom select

--- client/src/components/CustomSelect.tsx
+++ client/src/components/CustomSelect.tsx
@@ -2,79 +2,80 @@ import { useLayoutEffect, useRef, useState } from 'react'
 import styles from '../styles/CustomSelect.module.css'
 
 type Option = {
-    value: string
-    label?: string
+	value: string
+	label?: string
 }
 type CustomSelectProps = {
-    name?: string
-    defaultOption: Option
-    options: Option[]
-    className?: string
-    disabled?: boolean
-    onChange?: (option: Option) => void
+	name?: string
+	defaultOption: Option
+	options: Option[]
+	className?: string
+	disabled?: boolean
+	onChange?: (option: Option) => void
 }
 
 export default function CustomSelect({
-    name,
-    defaultOption,
-    options,
-    className,
-    disabled,
-    onChange,
+	name,
+	defaultOption,
+	options,
+	className,
+	disabled,
+	onChange,
 }: CustomSelectProps) {
-    const customSelectRef = useRef<HTMLDivElement>(null)
-    const [current, setCurrent] = useState(defaultOption)
-    useLayoutEffect(() => {
-        if (current.label == undefined) setCurrent(defaultOption)
-    }, [current, defaultOption])
-    return (
-        <div ref={customSelectRef}
-            onClick={() => {
-                if (disabled) return
-                customSelectRef.current?.classList.toggle(styles['hidden'])
-            }}
-            className={
-                [
-                    'input-d',
-                    styles['custom-select'],
-                    styles['hidden'],
-                    disabled ? styles['disabled'] : '',
-                    className
-                ].join(' ')
-            }
-        >
-            <span>{current.label}</span>
-            <input type="text"
-                name={name}
-                value={current.value}
-                disabled={disabled}
-                hidden
-                onChange={(e) => { e.preventDefault() }} />
-            <div
-                className={
-                    [
-                        'input-d',
-                        styles['select-dropbox'],
-                    ].join(' ')
-                }
-            >
-                {options.map(option => {
-                    return (
-                        <div key={option.value}
-                            onClick={() => {
-                                onChange && onChange(option)
-                                setCurrent(option)
-                            }}
-                            className={
-                                [
-                                    styles['select-item']
-                                ].join(' ')
-                            }>
-                            <span>{option.label}</span>
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+	const customSelectRef = useRef<HTMLDivElement>(null)
+	const [current, setCurrent] = useState(defaultOption)
+	useLayoutEffect(() => {
+		if (current.label == undefined) setCurrent(defaultOption)
+	}, [current, defaultOption])
+	return (
+		<div
+			onClick={() => {
+				if (disabled) return
+				customSelectRef.current?.classList.toggle(styles['hidden'])
+			}}
+			className={
+				[
+					'input-d',
+					styles['custom-select'],
+					disabled ? styles['disabled'] : '',
+					className
+				].join(' ')
+			}
+		>
+			<span>{current.label}</span>
+			<input type="text"
+				name={name}
+				value={current.value}
+				disabled={disabled}
+				hidden
+				onChange={(e) => { e.preventDefault() }} />
+			<div
+				ref={customSelectRef}
+				className={
+					[
+						'input-d',
+						styles['hidden'],
+						styles['select-dropbox'],
+					].join(' ')
+				}
+			>
+				{options.map(option => {
+					return (
+						<div key={option.value}
+							onClick={() => {
+								onChange && onChange(option)
+								setCurrent(option)
+							}}
+							className={
+								[
+									styles['select-item']
+								].join(' ')
+							}>
+							<span>{option.label}</span>
+						</div>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
