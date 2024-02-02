fb5c202faa4ef7cbceba3e444fb50b3636451641
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-02-02 08:01:13 +0700

    Update Typing

--- client/src/components/CustomSelect.tsx
+++ client/src/components/CustomSelect.tsx
@@ -2,7 +2,7 @@ import { useLayoutEffect, useRef, useState } from 'react'
 import styles from '../styles/CustomSelect.module.css'
 
 type Option = {
-	value: string
+	value: string | number
 	label?: string
 }
 type CustomSelectProps = {
