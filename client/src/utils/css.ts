89a33ff4304252cc30710df7e8fe7f86a986501f
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-05-30 12:44:00 +0700

    Update trim classNames

--- client/src/utils/css.ts
+++ client/src/utils/css.ts
@@ -1,3 +1,6 @@
 export default function css(...classNames: (string | undefined | null)[]) {
-	return classNames.join(' ').trim()
+	return classNames
+		.map(item => item?.trim())
+		.join(' ')
+		.trim()
 }
