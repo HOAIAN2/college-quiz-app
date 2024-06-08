ccecffd3d87cbe0fd82a8d401b0b0fc46e457bf6
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-06-08 14:36:29 +0700

    get user direct in Auth::user()

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 abstract class Controller extends BaseController
 {
@@ -26,6 +27,6 @@ abstract class Controller extends BaseController
 
 	public function getUser()
 	{
-		return request()->user();
+		return Auth::user();
 	}
 }
