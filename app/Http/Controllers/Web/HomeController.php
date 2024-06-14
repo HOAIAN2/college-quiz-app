e2186fcb61259c0fbabf98330fb037f75138c85d
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-06-14 16:23:43 +0700

    Remove Create  in HomeController

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -8,12 +8,6 @@ class HomeController extends Controller
 {
 	public function index()
 	{
-		$data = [];
-		$data['lang'] = app()->getLocale();
-		$data['description'] = trans('app.meta.description');
-		$data['keywords'] = trans('app.meta.keywords');
-		$data['title'] = config('app.name');
-		$data['app_url'] = config('app.url');
-		return view('index', $data);
+		return view('index');
 	}
 }
