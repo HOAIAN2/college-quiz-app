70ebd944642cd4d1b474376524494a2cbf8dedf8
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-07-21 09:40:28 +0700

    Clean code

--- app/Http/Middleware/RunTasks.php
+++ app/Http/Middleware/RunTasks.php
@@ -32,9 +32,7 @@ class RunTasks
 	{
 		$now = Carbon::now();
 
-		$last_run_tasks_at = Cache::has('last_run_tasks_at')
-			? Carbon::parse(Cache::get('last_run_tasks_at'))
-			: $now;
+		$last_run_tasks_at = Carbon::parse(Cache::get('last_run_tasks_at', $now->format('Y-m-d H:i:s')));
 		if ($last_run_tasks_at->addSeconds($run_tasks_interval)->lessThanOrEqualTo($now)) {
 			foreach ($this->tasks as $task) {
 				Artisan::call($task);
