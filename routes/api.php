3db7e52c18f683a8f12986e145676b3bd24d6d7e
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-07-21 16:33:57 +0700

    Clean code

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,6 @@ use App\Http\Controllers\Api\SubjectController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::prefix('/admin')->controller(AdminController::class)->group(function () {
 	Route::post('/run-artisan', 'runArtisan')->middleware(App\Http\Middleware\VerifyAppKey::class);
 	Route::middleware('auth:sanctum')->group(function () {
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,4 @@
 use App\Http\Controllers\Web\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::fallback([HomeController::class, 'index']);
