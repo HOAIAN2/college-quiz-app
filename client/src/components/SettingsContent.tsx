b26fc0ee45c01694cdcb3dbd9ba3e4a515b82077
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-07-31 08:59:54 +0700

    Update packages, seperate component in SettingsContent

--- client/src/components/SettingsContent.tsx
+++ client/src/components/SettingsContent.tsx
@@ -1,20 +1,46 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import { apiDeleteLogFile, apiDownloadLogFile, apiRunArtisan } from '../api/settings';
 import appStyles from '../App.module.css';
 import useAppContext from '../hooks/useAppContext';
 import useLanguage from '../hooks/useLanguage';
+import { UserDetail } from '../models/user';
 import styles from '../styles/SettingsContent.module.css';
 import apiUtils from '../utils/apiUtils';
 import css from '../utils/css';
 import { saveBlob } from '../utils/saveBlob';
 
+type LanguageType = ReturnType<typeof useLanguage<'component.settings_content'>>;
+
 export default function SettingsContent({ name }: { name: string; }) {
 	const { user } = useAppContext();
 	const language = useLanguage('component.settings_content');
-	const [artisanCommand, setArtisanCommand] = useState('');
+	if (!user.user) return null;
+	if (name === 'system') return <SystemContent user={user.user} language={language} />;
+	if (name === 'notifications') return (
+		<>
+		</>
+	);
+	return null;
+}
 
-	const handleRunArtisan = () => {
-		apiRunArtisan(artisanCommand);
+function SystemContent({
+	user,
+	language
+}: {
+	user: UserDetail;
+	language: LanguageType;
+}) {
+	const artisanCommandInputRef = useRef<HTMLInputElement>(null);
+
+	const handleRunArtisan = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		const command = artisanCommandInputRef.current?.value.trim();
+		if (!command) return;
+		const button = e.currentTarget;
+		button.classList.add(appStyles['button-submitting']);
+		apiRunArtisan(command)
+			.finally(() => {
+				button.classList.remove(appStyles['button-submitting']);
+			});
 	};
 
 	const handleDownloadLogFile = () => {
@@ -25,19 +51,17 @@ export default function SettingsContent({ name }: { name: string; }) {
 				saveBlob(res.data, fileName);
 			});
 	};
-
-	if (name === 'system') return (
+	return (
 		<>
 			{
-				user.user?.role.name === 'admin' ?
+				user.role.name === 'admin' ?
 					<>
 						<article className={styles['article']}>
 							<h3>{language?.artisancommand}</h3>
 							<p>{language?.runArtisanCommand}</p>
 							<label>{language?.command}</label>
 							<input
-								value={artisanCommand}
-								onInput={e => { setArtisanCommand(e.currentTarget.value); }}
+								ref={artisanCommandInputRef}
 								className={css(appStyles['input-d'], styles['input-item'])}
 								placeholder='schedule:run'
 							/>
@@ -66,10 +90,4 @@ export default function SettingsContent({ name }: { name: string; }) {
 			}
 		</>
 	);
-
-	if (name === 'notifications') return (
-		<>
-		</>
-	);
-	return null;
 }
