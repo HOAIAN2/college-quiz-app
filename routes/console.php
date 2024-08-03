55a0577059ec8df4516e51fe6b620f8cdc49a002
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-08-04 06:35:47 +0700

    Updtae console

--- routes/console.php
+++ routes/console.php
@@ -6,12 +6,10 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::call(function () {
 	Artisan::call('app:clear-unsed-tokens');
 	Artisan::call('app:cancel-late-exams');
-
-	Artisan::call('app:backup-database --once-per-day');
 })
 	->everyMinute();
 
-// Schedule::call(function () {
-// 	Artisan::call('app:backup-database');
-// })->name('Backup database')
-// 	->daily();
+Schedule::call(function () {
+	Artisan::call('app:backup-database');
+})->name('Backup database')
+	->daily();
