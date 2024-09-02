8b850dfd71461bc7f02504fdb345281731b15a7e
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-09-02 10:31:09 +0700

    Update Searchable Traits

--- backend/app/Traits/Searchable.php
+++ backend/app/Traits/Searchable.php
@@ -17,27 +17,28 @@ trait Searchable
 	 */
 	public function scopeSearch($query, $value, array $columns = [])
 	{
+		$search_against = "$value%";
 		if (empty($columns)) $columns = $this->searchable;
 		if (in_array('first_name', $columns) && in_array('last_name', $columns)) {
-			$query->where(DB::raw("CONCAT (last_name, ' ' , first_name)"), 'like', "%$value%")
-				->orWhere(DB::raw("CONCAT (first_name, ' ' , last_name)"), 'like', "%$value%");
+			$query->where(DB::raw("CONCAT (last_name, ' ' , first_name)"), 'like', $search_against)
+				->orWhere(DB::raw("CONCAT (first_name, ' ' , last_name)"), 'like', $search_against);
 		}
 
 		$first_column = array_shift($columns);
 		if (Str::contains($first_column, '.')) {
 			[$relation, $relationColumn] = explode('.', $first_column);
-			$query->whereHas($relation, function ($subQuery) use ($value, $relationColumn) {
-				$subQuery->where($relationColumn, 'like', "%$value%");
+			$query->whereHas($relation, function ($subQuery) use ($search_against, $relationColumn) {
+				$subQuery->where($relationColumn, 'like', $search_against);
 			});
-		} else $query->where($first_column, 'like', "%$value%");
+		} else $query->where($first_column, 'like', $search_against);
 
 		foreach ($columns as $column) {
 			if (Str::contains($column, '.')) {
 				[$relation, $relationColumn] = explode('.', $column);
-				$query->orWhereHas($relation, function ($subQuery) use ($value, $relationColumn) {
-					$subQuery->where($relationColumn, 'like', "%$value%");
+				$query->orWhereHas($relation, function ($subQuery) use ($search_against, $relationColumn) {
+					$subQuery->where($relationColumn, 'like', $search_against);
 				});
-			} else $query->orWhere($column, 'like', "%$value%");
+			} else $query->orWhere($column, 'like', $search_against);
 		}
 		return $query;
 	}
