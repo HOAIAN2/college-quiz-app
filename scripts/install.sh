763b2fa6e2970c7b9d66aaed2d181114cb71f8fc
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-10-26 09:20:35 +0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 cd $ROOT_DIR
-cd backend
-composer i
-composer run-script post-root-package-install
-composer run-script post-create-project-cmd
-cd $ROOT_DIR
-cd frontend
-npm i
-cp .env.example .env
-cp docker/nginx/nginx.example.conf docker/nginx/nginx.conf
-cp docker/php/zz-docker.example.conf docker/php/zz-docker.conf
+
+if [[ $1 = '--docker' ]] ; then
+    cp -n docker/nginx/nginx.example.conf docker/nginx/nginx.conf
+    cp -n docker/php/zz-docker.example.conf docker/php/zz-docker.conf
+else
+    cd backend
+    composer i
+    composer run-script post-root-package-install
+    composer run-script post-create-project-cmd
+    cd $ROOT_DIR
+    cd frontend
+    npm i
+    cp -n .env.example .env
+fi
