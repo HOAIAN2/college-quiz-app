9cdc9f0893b09d622738aeeaa4911433464a06a4
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-10-28 07:40:36 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 cd $ROOT_DIR
-cd frontend
-npm run build
-cd $ROOT_DIR
-cd backend
-rm public/index.html
-rm public/favicon.ico
-rm -rf public/assets
-cp -r ../frontend/dist/* public
-echo 'Copying distribution to server...'
-if [ ! -d ./resources/views ]; then
-    mkdir ./resources/views
+
+if [[ $1 = '--docker' ]] ; then
+    docker compose build --no-cache
+else
+    cd frontend
+    npm run build
+    cd $ROOT_DIR
+    cd backend
+    rm public/index.html
+    rm public/favicon.ico
+    rm -rf public/assets
+    cp -r ../frontend/dist/* public
+    echo 'Copying distribution to server...'
+    if [ ! -d ./resources/views ]; then
+        mkdir ./resources/views
+    fi
+    cp -r ./public/index.html ./resources/views/index.blade.php
+    echo 'Completed!'
 fi
-cp -r ./public/index.html ./resources/views/index.blade.php
-echo 'Completed!'
