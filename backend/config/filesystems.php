742b009ac3c927cb91c2fbe28547fa568ff454cf
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-11-27 11:21:21 +0700

    Add upload disk

--- backend/config/filesystems.php
+++ backend/config/filesystems.php
@@ -56,6 +56,12 @@ return [
             'throw' => false,
         ],
 
+        'upload' => [
+            'driver' => 'local',
+            'root' => storage_path('uploads'),
+            'throw' => false,
+        ],
+
     ],
 
     /*
