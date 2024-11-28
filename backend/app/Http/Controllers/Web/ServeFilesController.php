8899c23482e6dc372be4278b4a0e16f374b080d9
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-11-28 16:42:44 +0700

    Only return image files

--- backend/app/Http/Controllers/Web/ServeFilesController.php
+++ backend/app/Http/Controllers/Web/ServeFilesController.php
@@ -8,18 +8,20 @@ use Illuminate\Support\Facades\Storage;
 
 class ServeFilesController extends Controller
 {
-    public function uploads(string $filename)
+    public function uploads(string $file_name)
     {
-        if (Storage::exists($filename)) {
-            $mimeType =  Storage::mimeType($filename);
+        $allowed_mime_types = ['image/jpeg', 'image/png', 'image/gif', 'image/webp', 'image/bmp', 'image/svg+xml'];
 
-            // Retrieve the file contents
-            $file = Storage::get($filename);
-            return response()->make($file, 200, [
-                'Content-Type' => $mimeType,
-                'Cache-Control' => 'public, max-age=86400', // Cache for 1 day (86400 seconds)
-                'Expires' => now()->addDay()->toRfc1123String(),
-            ]);
+        if (Storage::exists($file_name)) {
+            $mime_type =  Storage::mimeType($file_name);
+            if (in_array($mime_type, $allowed_mime_types)) {
+                $file = Storage::get($file_name);
+                return response()->make($file, 200, [
+                    'Content-Type' => $mime_type,
+                    'Cache-Control' => 'public, max-age=86400', // Cache for 1 day (86400 seconds)
+                    'Expires' => now()->addDay()->toRfc1123String(),
+                ]);
+            } else abort(404);
         }
 
         return abort(404);
