4db81f99f1075b6575ed2391066d4cd61aaee299
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2024-12-04 12:03:56 +0700

    Remove useless function

--- backend/app/Helper/DOMStringHelper.php
+++ backend/app/Helper/DOMStringHelper.php
@@ -7,66 +7,8 @@ use Illuminate\Support\Facades\Storage;
 
 class DOMStringHelper
 {
-    public static function saveImagesFromDOM(string $html_string)
-    {
-        libxml_use_internal_errors(true);
-        $html_string = mb_convert_encoding($html_string, 'UTF-8', 'auto');
-        $dom = new \DOMDocument();
-        @$dom->loadHTML(mb_convert_encoding($html_string, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-        libxml_clear_errors();
-        $images = $dom->getElementsByTagName('img');
-
-        $result = [];
-
-        foreach ($images as $img) {
-            $src = $img->attributes['src']->textContent;
-
-            if (preg_match('/^data:image\/(\w+);base64,/', $src, $matches)) {
-                $image_data = substr($src, strpos($src, ',') + 1);
-                $decoded_image = base64_decode($image_data);
-
-                $image_name = (string) Str::uuid() . '-' . time() . '.' . $matches[1];
-                $image_path = '' . $image_name;
-
-                Storage::put($image_path, $decoded_image);
-
-                $result[] = $image_path;
-            }
-        }
-        return $result;
-    }
-
     /**
-     * Only save first one
-     */
-    public static function saveImageFromDOM(string $html_string)
-    {
-        libxml_use_internal_errors(true);
-        $html_string = mb_convert_encoding($html_string, 'UTF-8', 'auto');
-        $dom = new \DOMDocument();
-        @$dom->loadHTML(mb_convert_encoding($html_string, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-        libxml_clear_errors();
-        $images = $dom->getElementsByTagName('img');
-
-        foreach ($images as $img) {
-            $src = $img->attributes['src']->textContent;
-
-            if (preg_match('/^data:image\/(\w+);base64,/', $src, $matches)) {
-                $image_data = substr($src, strpos($src, ',') + 1);
-                $decoded_image = base64_decode($image_data);
-
-                $image_name = (string) Str::uuid() . '-' . time() . '.' . $matches[1];
-                $image_path = '' . $image_name;
-
-                Storage::put($image_path, $decoded_image);
-
-                return $image_path;
-            }
-        }
-    }
-
-    /**
-     * Save images, replace base64 url with image path
+     * Save all base64 images, replace base64 url with image path
      */
     public static function processImagesFromDOM(string $html_string)
     {
@@ -92,27 +34,6 @@ class DOMStringHelper
                 $img->attributes['src']->textContent = "/uploads/$image_name";
             }
         }
-        return $dom->saveHTML();
-    }
-
-    public static function removeMediaNodes($html_string)
-    {
-        $dom = new \DOMDocument();
-        libxml_use_internal_errors(true);
-        $dom->loadHTML($html_string, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-        libxml_clear_errors();
-
-        $tags_to_remove = ['img', 'video', 'audio'];
-
-        foreach ($tags_to_remove as $tag) {
-            $elements = $dom->getElementsByTagName($tag);
-
-            while ($elements->length > 0) {
-                $element = $elements->item(0);
-                $element->parentNode->removeChild($element);
-            }
-        }
-
         return $dom->saveHTML();
     }
 }
