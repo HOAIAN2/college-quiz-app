c08d43f4a680a8af82aee334d6a8680bc4756259
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-01-26 08:05:21 +0700

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,7 @@ cd $ROOT_DIR
 if [[ $1 = '--docker' ]] ; then
     cp -n docker/nginx/nginx.example.conf docker/nginx/nginx.conf
     cp -n docker/php/zz-docker.example.conf docker/php/zz-docker.conf
+    cp -n docker-php-ext-opcache.example.ini docker/php/docker-php-ext-opcache.ini
 else
     cd backend
     composer i
