86ce956691f3cf70827d65eae47d20485f23c2ee
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-02-03 09:49:41 +0700

    Use PDO::ATTR_PERSISTENT, optimzie first query performance by sleep connect instead of reconnect everytime

--- backend/config/database.php
+++ backend/config/database.php
@@ -56,6 +56,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_PERSISTENT => true,
+                // PDO::MYSQL_ATTR_INIT_COMMAND => 'SET SESSION sql_mode="STRICT_TRANS_TABLES"',
             ]) : [],
         ],
 
@@ -76,6 +78,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::ATTR_PERSISTENT => true,
+                // PDO::MYSQL_ATTR_INIT_COMMAND => 'SET SESSION sql_mode="STRICT_TRANS_TABLES"',
             ]) : [],
         ],
 
