044d7386c9d7e9cd3aeeca75e93983c510adac63
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-02-08 08:55:38 +0700

    Update scripts - set exit on error -> prevent delete files when run wrong command. - remove vendor folder before re-install (fix error file size when update backend packages)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+set -e
+
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 $SCRIPT_DIR/build.sh
 cd $ROOT_DIR
 rm app.tar.gz
 cd backend
+rm -rf vendor
 composer install --optimize-autoloader --no-dev
 php artisan optimize:clear
 rm -f storage/logs/laravel.log
