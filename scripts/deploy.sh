d7a3937cfd3a001966378dac09a2b47043b7575f
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-02-09 12:15:59 +0700

    Update deploy script for docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 set -e
-
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.." &> /dev/null && pwd )
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$SCRIPT_DIR/build.sh
-cd $ROOT_DIR
-rm app.tar.gz
-cd backend
-rm -rf vendor
-composer install --optimize-autoloader --no-dev
-php artisan optimize:clear
-rm -f storage/logs/laravel.log
-rm -rf storage/framework/sessions/*
-echo 'Compressing...'
-tar --exclude=node_modules --exclude=dump --exclude=storage/uploads -czf ../app.tar.gz * .env.example
-echo 'Completed!'
-composer i
-cd $ROOT_DIR
+
+if [[ $1 = '--docker' ]] ; then
+    cd $ROOT_DIR
+    docker compose down
+    $SCRIPT_DIR/build.sh --docker
+    docker compose up -d
+else
+    $SCRIPT_DIR/build.sh
+    cd $ROOT_DIR
+    rm app.tar.gz
+    cd backend
+    rm -rf vendor
+    composer install --optimize-autoloader --no-dev
+    php artisan optimize:clear
+    rm -f storage/logs/laravel.log
+    rm -rf storage/framework/sessions/*
+    echo 'Compressing...'
+    tar --exclude=node_modules --exclude=dump --exclude=storage/uploads -czf ../app.tar.gz * .env.example
+    echo 'Completed!'
+    composer i
+    cd $ROOT_DIR
+fi
