590a1f89bd5578d48e377155cd71f930f460945e
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-02-14 15:58:07 +0700

    use REDIS_PERSISTENT env variable

--- backend/config/database.php
+++ backend/config/database.php
@@ -149,7 +149,7 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
-            'persistent' => true,
+            'persistent' => env('REDIS_PERSISTENT', true),
         ],
 
         'default' => [
