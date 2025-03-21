56687bc73b81a67117962674709483ed50c6100b
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-03-21 13:05:14 +0700

    optimize app:cleanup-images command

--- backend/app/Console/Commands/CleanupImages.php
+++ backend/app/Console/Commands/CleanupImages.php
@@ -6,7 +6,6 @@ use App\Models\Question;
 use App\Models\QuestionOption;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Str;
 
 class CleanupImages extends Command
 {
@@ -33,13 +32,13 @@ class CleanupImages extends Command
 
         Question::withTrashed()->select('content')->chunk(100, function ($questions) use (&$question_images) {
             foreach ($questions as $question) {
-                array_push($question_images, ...$this->extractImagePathsFromContent($question->content));
+                array_push($question_images, ...$question->getImages());
             }
         });
 
         QuestionOption::withTrashed()->select('content')->chunk(100, function ($options) use (&$question_images) {
             foreach ($options as $option) {
-                array_push($question_images, ...$this->extractImagePathsFromContent($option->content));
+                array_push($question_images, ...$option->getImages());
             }
         });
 
@@ -52,35 +51,5 @@ class CleanupImages extends Command
         });
 
         Storage::delete($files_to_delete);
-    }
-
-    /**
-     * This function not work in expected behavior because the getContentAttribute($value) function
-     * in Question and QuestionOption model will override all /uploads image path
-     * when code not running in console
-     */
-    protected function extractImagePathsFromContent(string $content): array
-    {
-        $image_paths = [];
-        libxml_use_internal_errors(true);
-        $html_string = mb_convert_encoding($content, 'UTF-8', 'auto');
-        $dom = new \DOMDocument();
-        @$dom->loadHTML(mb_convert_encoding($html_string, 'HTML-ENTITIES', 'UTF-8'), LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-        libxml_clear_errors();
-        $images = $dom->getElementsByTagName('img');
-
-        // Replace token when command not run in console (throught api call, etc)
-        $replace_token = app()->runningInConsole() ?
-            '/uploads/'
-            : request()->schemeAndHttpHost() . '/uploads/';
-
-        foreach ($images as $img) {
-            $src = $img->attributes['src']->textContent;
-            if (Str::startsWith($src, $replace_token)) {
-                $image_paths[] = Str::replace($replace_token, '', $src);
-            }
-        }
-
-        return $image_paths;
     }
 }
