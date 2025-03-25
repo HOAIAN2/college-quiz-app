3b7a8b485667aab627b79566c4a783e37048da4a
Lê Hoài Ân <98139595+HOAIAN2@users.noreply.github.com>
2025-03-25 07:38:25 +0700

    - ignore check mimetype on serve files. - update packages

--- backend/app/Http/Controllers/Web/ServeFilesController.php
+++ backend/app/Http/Controllers/Web/ServeFilesController.php
@@ -8,22 +8,9 @@ use Illuminate\Support\Facades\Storage;
 
 class ServeFilesController extends Controller
 {
-    protected array $allowedMimeTypes = [
-        'image/jpeg',
-        'image/png',
-        'image/gif',
-        'image/webp',
-        'image/bmp',
-        'image/svg+xml'
-    ];
-
     public function uploads(string $file_name)
     {
         try {
-            $mime_type = Storage::mimeType($file_name);
-            if (!in_array($mime_type, $this->allowedMimeTypes)) {
-                abort(403);
-            }
             return Storage::response(
                 $file_name,
                 null,
